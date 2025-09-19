66c0574bdabb8dc42c5d8bc3c0797148357b378d
sodiqbhoy1 <adamubabasodiq@gmail.com>
2025-09-19 14:24:43 +0100

    style: Clean up formatting and ensure consistent import statements in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,57 @@
-import { Route, Routes } from "react-router"
-import Homepage from "./components/Homepage"
-import AdminHome from "./admin/AdminHome"
-import AdminLogin from "./admin/adminLogin"
-import AdminSignup from "./admin/AdminSignup"
-import ForgotPassword from "./admin/ForogtPassword"
-import ResetPassword from "./admin/ResetPassword"
-import LandingPage from "./components/LandingPage"
-import ChatRoom from "./components/ChatRoom"
-import Rooms from "./admin/Rooms"
-import Profile from "./admin/Profile"
-import AdminDashboard from "./admin/AdminDashboard"
-import ProtectedRoute from "./admin/ProtectedRoute"
-import Dashboard from "./admin/Dashboard"
-import NotFound from "./components/NotFound"
-import Users from "./admin/Users"
+import { Route, Routes } from "react-router";
+import Homepage from "./components/Homepage";
+import AdminHome from "./admin/AdminHome";
+import AdminLogin from "./admin/adminLogin";
+import AdminSignup from "./admin/AdminSignup";
+import ForgotPassword from "./admin/ForogtPassword";
+import ResetPassword from "./admin/ResetPassword";
+import LandingPage from "./components/LandingPage";
+import ChatRoom from "./components/ChatRoom";
+import Rooms from "./admin/Rooms";
+import Profile from "./admin/Profile";
+import AdminDashboard from "./admin/AdminDashboard";
+import ProtectedRoute from "./admin/ProtectedRoute";
+import Dashboard from "./admin/Dashboard";
+import NotFound from "./components/NotFound";
+import Users from "./admin/Users";
 
 const App = () => {
   return (
     <>
+      <Routes>
+        <Route path="/" element={<Homepage/>} />
+        {/* <Route path='/about' element={<AboutPage/>}/> */}
+        <Route path="/admin/home" element={<AdminHome/>} />
+        <Route path="/admin/login" element={<AdminLogin/>} />
+        <Route path="/admin/signup" element={<AdminSignup/>} />
+        <Route path="/admin/forgot-password" element={<ForgotPassword/>} />
+        <Route path="/admin/reset-password/:token"
+          element={<ResetPassword/>}
+        />
 
-<Routes>
-  <Route path='/' element={<Homepage/>}/>
-  {/* <Route path='/about' element={<AboutPage/>}/> */}
-  <Route path='/admin/home' element={<AdminHome/>}/>
-<Route path='/admin/login' element={<AdminLogin/>}/>
-<Route path='/admin/signup' element={<AdminSignup/>}/>
-<Route path="/admin/forgot-password" element={<ForgotPassword />} />
- <Route path="/admin/reset-password/:token" element={<ResetPassword/>} />
+        <Route path="/join/:roomId" element={<LandingPage/>} />
+        <Route path="/chat/:roomId" element={<ChatRoom/>} />
 
-<Route path="/join/:roomId" element={<LandingPage />} />
-<Route path="/chat/:roomId" element={<ChatRoom />} />
+        {/* admin dashboard now protected */}
+        <Route
+          path="/admin-dashboard/*"
+          element={
+            <ProtectedRoute>
+              <AdminDashboard/>
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Dashboard/>} />
+          <Route path="rooms" element={<Rooms/>} />
+          <Route path="users" element={<Users/>} />
+          <Route path="profile" element={<Profile/>} />
+        </Route>
 
-{/* admin dashboard now protected */}
-<Route 
-        path='/admin-dashboard/*' 
-        element={
-          <ProtectedRoute>
-            <AdminDashboard />
-          </ProtectedRoute>
-        } 
-      >
-        <Route index element={<Dashboard/>} />
-        <Route path='rooms' element={<Rooms />} />
-        <Route path='users' element={<Users />} />
-        <Route path='profile' element={<Profile />} />
-     </Route>
-
-     {/* 404 Not Found - This should be the last route */}
-  <Route path='*' element={<NotFound/>} />
-     
-
-
-</Routes>
+        {/* 404 Not Found - This should be the last route */}
+        <Route path="*" element={<NotFound/>} />
+      </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
