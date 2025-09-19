3543bb1a18627b73399c5a30b10ed3ddbbf707ab
sodiqbhoy1 <adamubabasodiq@gmail.com>
2025-09-19 14:45:26 +0100

    fix: Correct Admin login route to use AdminSignin component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Homepage from "./components/Homepage";
 import AdminHome from "./admin/AdminHome";
-import AdminLogin from "./admin/adminLogin";
 import AdminSignup from "./admin/AdminSignup";
 import ForgotPassword from "./admin/ForogtPassword";
 import ResetPassword from "./admin/ResetPassword";
@@ -14,15 +13,15 @@ import ProtectedRoute from "./admin/ProtectedRoute";
 import Dashboard from "./admin/Dashboard";
 import NotFound from "./components/NotFound";
 import Users from "./admin/Users";
+import AdminSignin from "./admin/AdminSignin";
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Homepage/>} />
-        {/* <Route path='/about' element={<AboutPage/>}/> */}
         <Route path="/admin/home" element={<AdminHome/>} />
-        <Route path="/admin/login" element={<AdminLogin/>} />
+        <Route path="/admin/login" element={<AdminSignin/>} />
         <Route path="/admin/signup" element={<AdminSignup/>} />
         <Route path="/admin/forgot-password" element={<ForgotPassword/>} />
         <Route path="/admin/reset-password/:token"
